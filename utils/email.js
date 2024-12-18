5872d63eb9260c13bca42183909bafbd85da3620
Olukemi Fasuan <ayanfe1711@gmail.com>
2024-12-18 07:40:04 +0100

    made changes to email verification utility

--- utils/email.js
+++ utils/email.js
@@ -17,10 +17,47 @@ const sendGmail = async (email, emailVerificationToken) => {
     from: `"Dwella Team" <${process.env.EMAIL_USER}>`,
     to: `${email}`,
     subject: 'Verify your email address',
-    html: `<p>Hi,</p>
-    <br>
-    <p>Thanks for signing up! Please verify your email address by clicking the link below</p>
-    <a href="http://localhost:5000/v1/users/verify-email?emailVerificationToken=${emailVerificationToken}"  style="
+    html: `
+    <html>
+    <head>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          text-align: left; /* Left-align the text */
+          font-size: 18px; /* Increase the font size */
+        }
+        .email-body {
+          width: 100%;
+          max-width: 600px;
+          margin: 0 auto; /* Center the entire content */
+          text-align: left; /* Left-align the content */
+        }
+        h1 {
+          font-size: 24px;
+        }
+        p {
+          font-size: 18px;
+        }
+        a {
+          display: inline-block;
+          background-color: #007BFF;
+          color: white;
+          padding: 10px 20px;
+          text-decoration: none;
+          font-size: 18px;
+          border-radius: 5px;
+          margin-top: 20px;
+        }
+        a:hover {
+          background-color: #0056b3;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="email-body">
+        <h1>Hi,</h1>
+        <p>Thanks for signing up! Please verify your email address by clicking the link below:</p>
+        <a href="http://localhost:5000/v1/users/verify-email?emailVerificationToken=${emailVerificationToken}"  style="
              display: inline-block;
              padding: 7px 14px;
              color: #ffffff; 
@@ -28,14 +65,22 @@ const sendGmail = async (email, emailVerificationToken) => {
              text-decoration: none; 
              border-radius: 5px;
            ">Verify Email</a>
-    <p>If you did not request this, please ignore this email.</p>`
-  };
+        <p>If you did not request this, please ignore this email.</p>
+       <p style="margin-bottom: 0; padding-bottom: 0;">
+          Thank you,<br>
+          The Dwella Team
+      </p>
+      </div>
+    </body>
+  </html>
+  `
+};
 
   transporter.sendMail(mailOptions, function(error, info) {
     if (error) {
       return console.log(error)
     } else {
-      console.log('Email sent: ' + info.response)
+      console.log('Verification Email sent: ' + info.response)
     }
   });
 }
